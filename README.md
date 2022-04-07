0b87a5d18b07c627f09902cccd343a03098eb1fd
ezYakaEagle442 <stepin.cloud@outlook.com>
2022-04-07 22:35:50 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # spring-cloud-az-kv
+
+Read [https://github.com/Azure-Samples/azure-spring-boot-samples/tree/spring-cloud-azure_4.0.0-beta.2/keyvault/spring-cloud-azure-starter-keyvault-secrets/single-property-source](https://github.com/Azure-Samples/azure-spring-boot-samples/tree/spring-cloud-azure_4.0.0-beta.2/keyvault/spring-cloud-azure-starter-keyvault-secrets/single-property-source)
+
---
+++ src/main/java/com/example/azure/spring/keyvault/secrets/sample/SampleApplication.java
@@ -0,0 +1,25 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.example.azure.spring.keyvault.secrets.sample;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleApplication implements CommandLineRunner {
+
+    @Value("${MYSQL-SERVER-FULL-NAME}")
+    private String springDataSourceUrl;
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleApplication.class, args);
+    }
+
+    public void run(String[] args) {
+        System.out.println("Secret MYSQL-SERVER-FULL-NAME in Azure Key Vault: " + springDataSourceUrl);
+    }
+
+}
