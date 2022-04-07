7c4872a119611de907a2bb64e2fac8ce5d41ee2b
ezYakaEagle442 <stepin.cloud@outlook.com>
2022-04-07 22:44:43 +0200

    corrected test package path

---
+++ src/test/java/com/example/azure/spring/keyvault/secrets/sample/SampleApplicationTests.java
@@ -0,0 +1,16 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.example.azure.spring.keyvault.secrets.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
